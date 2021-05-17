b03489ec3ab86eb8da3d845eb9b464b934b2c062
Soniinho <mh.daltroso@gmail.com>
2021-05-17 09:38:13 -0300

    confirmar em 'main'

---
+++ vetor 05.17/2.c
@@ -0,0 +1,43 @@
+#include <stdio.h>  //f6 para compilar
+#include <stdlib.h> //encoding windows 1252(Western)
+#include <locale.h> //fold all comments
+
+int main()
+{
+    setlocale(LC_ALL, "portuguese");
+    
+    int n[6], i;
+
+    printf("Será pedido para digitar 7 números. Aperte \"enter\" para continuar...");
+    getchar();
+    system("cls");
+
+    for(i = 0; i < 6; i++){
+        printf("Digite o %d° número: ", i + 1);
+        scanf("%d%*c", &n[i]);
+    }
+
+    printf("\nMúltiplos de 2: \n");
+    for(i = 0; i < 6; i++){
+        if(n[i] % 2 == 0){
+            printf("O %d° é: %d\n", i + 1, n[i]);
+        }
+    }
+
+    printf("\nMúltiplos de 3: \n");
+    for(i = 0; i < 6; i++){
+        if(n[i] % 3 == 0){
+            printf("O %d° é: %d\n", i + 1, n[i]);
+        }
+    }
+
+    printf("\nMúltiplos de 2 e 3: \n");
+    for(i = 0; i < 6; i++){
+        if(n[i] % 2 == 0 && n[i] % 3 == 0){
+            printf("O %d° é: %d\n", i + 1, n[i]);
+        }
+    }
+
+    system("pause");
+    return 0;
+}
